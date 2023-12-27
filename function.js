00d0ef9a8436d326fee0e394d4c7ceb78843c876
haneul <tarabut@naver.com>
2023-12-27 19:12:46 +0900

    Function(함수), Arrow Function

---
+++ function.js
@@ -0,0 +1,176 @@
+// Function
+// - fundamental building block in the program
+// - subprogram can be used multiple times
+// - performs a task or calculates a value
+
+// 1. Function declaration
+// function name(param1, param2) { body... return; }
+// one function === one thing // 하나의 함수는 하나의 일만 작동해야한다.
+// naming: doSomething, command, verb // 함수 이름 지정방식
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS // !자바스크립트에서 함수는 오브젝트이다.
+function printHello() {
+    console.log('Hello');
+}
+printHello();
+
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
+
+// 2. Parameters(매개변수)
+// primitive parameters: passed by value
+// object parameters: passed by reference
+function changeName(obj) {
+    obj.name = 'coder';
+}
+const han = {
+    name: 'ellie'
+};
+changeName(han);
+console.log(han);
+
+// 3. Default parameters (added in ES6)(매개변수 기본값)
+function showMessage(message, from = 'unknown') {
+    console.log(`${message} by ${from}`);
+}
+showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6) (나머지 매개변수(가변인자?))
+// 배열 형태로 전달된다.
+function printAll(...args) {
+    for (let i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+    // for of 문법
+    for (const arg of args) {
+        console.log(arg);
+    }
+
+    args.forEach((arg) => console.log(arg));
+}
+printAll('Hi', 'im', 'Han');
+
+// 5. Local scope (지역변수?)
+let globalMessage = 'global'; // global variable
+function printMessage() {
+    let message = 'hello';
+    console.log(message); // local variable
+    console.log(globalMessage);
+
+    function printAnother() {
+        console.log(message);
+        let childMessage = 'hello';
+    }
+    // console.log(childMessage); //error
+}
+printMessage();
+// console.log(message); // 에러 발생
+
+// 6. Return a value
+function sum(a, b) {
+    return a + b;
+}
+const result = sum(1, 2); // 3
+console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        // long upgrade logic...
+    }
+}
+
+// good
+function upgradeUser(user) {
+    if (user.point <= 10) {
+        return;
+    }
+    // long upgrade logic...
+}
+
+// First-class function(변수와 마찬가지로 변수에 할당이되고 매개변수,반환값 가능)
+// functions are treated like any other variable
+// can be assigned as a value to variable
+// can be passed as an argument to other functions.
+// can be returned by another function
+
+// 1. Function expression(함수를 변수에 할당)
+// a function declaration can be called earlier than it is defined. (hoisted)
+// a function expression is created when the execution reaches it.
+const print = function() { // anonymous function(이름없는 함수)
+    console.log('print');
+};
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// 2. Callback function using function expression
+// 다른 함수에게 인자로 전달되어 나중에 실행되는 함수
+function randomQuiz(answer, printYes, printNo) {
+    if (answer === 'love you') {
+        printYes();
+    } else {
+        printNo();
+    }
+}
+// anonymous function
+const printYes = function() {
+    console.log('yes!');
+};
+
+// named function
+// better debugging in debugger's stack traces
+// recursions
+const printNo = function print() {
+    console.log('no!');
+};
+randomQuiz('wrong', printYes, printNo);
+randomQuiz('love you', printYes, printNo);
+
+// Arrow function
+// always anonymous
+
+// const simplePrint = function () {
+//   console.log('simplePrint!');
+// };
+// 바로 위의 Function expression(함수를 변수에 할당)을 간편히 아래(Arrow function)으로 간단히 가능
+
+const simplePrint = () => console.log('simplePrint!');
+const add = (a, b) => a + b;
+const simpleMultiply = (a, b) => {
+    // do something more
+    return a * b;
+};
+
+// IIFE: Immediately Invoked Function Expression
+// 선언함과 동시에 함수 호출 (요즘에는 잘 안쓰임)
+(function hello() {
+    console.log('IIFE');
+})();
+
+// Fun quiz time
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
+function calculate(command, a, b) {
+    switch (command) {
+        case 'add':
+            return a + b;
+        case 'substract':
+            return a - b;
+        case 'divide':
+            return a / b;
+        case 'multiply':
+            return a * b;
+        case 'remainder':
+            return a % b;
+        default:
+            throw Error('unknown command');
+    }
+}
+console.log(calculate('substract', 3, 3));
